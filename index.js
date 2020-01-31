02f4ce9af5b8b721c390f9e2f910e528bb89db2b
Shawn Ma <shawnma@uber.com>
2020-01-31 10:25:15 -0800

    callback handlers. not working well

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const device = puppeteer.devices['iPad Pro landscape'];
 const LOOKUP = {
   origin: 'SFO',
   dest: 'YVR',
-  datetime: '2020-05-08T12:00',
+  datetime: '2020-08-07T12:00',
   flight: 'WS1775'
 };
 
@@ -16,14 +16,24 @@ function extractedBookId(searchResponse) {
   return bid[1];
 }
 
+function debug(s) {
+  if (true) {
+    console.log(s);
+  }
+}
+
 async function extractFlight(searchResponse) {
-  const searchJson = await searchResponse.json();
+  debug("extractFlight");
+  const searchJson = searchResponse; //await searchResponse.json();
+  debug("Got JSON");
   const options = searchJson['flights'][0]['flightOptions'];
   let matchingFlight = null;
   options.forEach((option) => {
-    const flight = option['flightDetails']['flightSegments'];
-    if (flight.length === 1 && (flight[0]['marketingAirline'] + flight[0]['flightNumber']) === LOOKUP.flight) {
-      matchingFlight = flight[0];
+    const segment = option['flightDetails']['flightSegments'];
+    if (segment.length === 1
+      && (segment[0]['marketingAirline'] + segment[0]['flightNumber']) === LOOKUP.flight) {
+      // && segment[0].departureDateRaw.startsWith(LOOKUP.datetime)
+      matchingFlight = segment[0];
     }
   });
   return matchingFlight;
@@ -32,7 +42,7 @@ async function extractFlight(searchResponse) {
 function encodeSeatsUrl(matchingFlight, bookId) {
   const flight = encodeURI(JSON.stringify([{
     flight: [{
-      "fareClass": "B",
+      "fareClass": "E",
       "flightNumber": matchingFlight.flightNumber,
       "airlineCodeOperating": matchingFlight.operatingAirline,
       "operatingFlightNumber": matchingFlight.operatingAirlineFlightNumber,
@@ -72,21 +82,55 @@ void (async () => {
     });
   });
 
+  let pageListeners = {};
+  page.on('response', async response => {
+    debug("got response " + response.url());
+    for (const url in pageListeners) {
+      if (response.url().indexOf(url) > 0) {
+        debug(response.status());
+        debug(response.url());
+        const json = await response.json();
+        pageListeners[url](json, response);
+        break;
+      }
+    }
+  });
+
+  function t(url) {
+    let resolveCallback, rejectCallback;
+    pageListeners[url] = function (json, response) {
+      resolveCallback([json, response]);
+    };
+    return new Promise((resolve, reject) => {
+      resolveCallback = resolve;
+      rejectCallback = reject;
+    });
+  }
+
   const date = LOOKUP.datetime.split("T")[0];
   let shop = `https://www.westjet.com/shop/?lang=en&type=search&origin=${LOOKUP.origin}&destination=${LOOKUP.dest}&adults=1&children=0&infants=0&outboundDate=${date}&returnDate=&companionvoucher=false&iswestjetdollars=false&promo=&currency=USD&caller=https%3A%2F%2Fwww.westjet.com%2Fen-us%2Findex`;
-  const shopPromise = page.goto(shop);
-  const searchResponse = await page.waitForResponse(r=>r.url().indexOf("/bookingservices/flightSearch") > 0 && r.ok());
-  const bookId = extractedBookId(searchResponse);
-  const matchingFlight = await extractFlight(searchResponse);
-  console.log(matchingFlight);
-  console.log(`book id = ${bookId}`);
-  await shopPromise;
+  const searchPromise = t("/bookingservices/flightSearch");
+  await page.goto(shop);
+  const searchResponse = await searchPromise;// page.waitForResponse(r => r.url().indexOf("/bookingservices/flightSearch") > 0, 3000);
 
-  const url = encodeSeatsUrl(matchingFlight, bookId);
-  console.log(url);
-  const pageLoad = page.goto(url);
-  const resp = await page.waitForResponse(r => r.url().indexOf("/extendedSeatmap") > 0);
+  const bookId = extractedBookId(searchResponse[1]);
+  debug(bookId);
+  const matchingFlight = await extractFlight(searchResponse[0]);
+  if (matchingFlight == null) {
+    console.log("Unable to find flight");
+    return;
+  }
+  debug(matchingFlight);
+  const seatsPromise = page.goto(encodeSeatsUrl(matchingFlight, bookId));
+  const resp = await page.waitForResponse(r => r.url().indexOf("/extendedSeatmap") > 0, 3000);
   const json = await resp.json();
-  console.log(json);
-  await pageLoad;
+  json.aircraft.deck.cabin.rows.forEach(row => {
+    let info = ("" + row.rowNumber).padStart(2, ' ') + "   "
+      + row.seats.map(s =>
+        (s.seatNumber + " " + ((s.characteristics.NO_SEAT || s.characteristics.OFFERED_LAST) ? "---" : s.prices[0]['price'])).padStart(15, ' ')
+      ).join(" ");
+    console.log(info);
+  });
+  await seatsPromise;
+  await browser.close();
 })();
