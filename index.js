a33414e238f1801c75fa1d07a0ae7a518b3aeae7
Shawn Ma <shawnma@uber.com>
2020-01-31 10:33:50 -0800

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const device = puppeteer.devices['iPad Pro landscape'];
 
-const LOOKUP = {
+const lookup = {
   origin: 'SFO',
   dest: 'YVR',
   datetime: '2020-08-07T12:00',
@@ -17,21 +17,21 @@ function extractedBookId(searchResponse) {
 }
 
 function debug(s) {
-  if (true) {
+  if (false) {
     console.log(s);
   }
 }
 
 async function extractFlight(searchResponse) {
   debug("extractFlight");
-  const searchJson = searchResponse; //await searchResponse.json();
+  const searchJson = await searchResponse.json();
   debug("Got JSON");
   const options = searchJson['flights'][0]['flightOptions'];
   let matchingFlight = null;
   options.forEach((option) => {
     const segment = option['flightDetails']['flightSegments'];
     if (segment.length === 1
-      && (segment[0]['marketingAirline'] + segment[0]['flightNumber']) === LOOKUP.flight) {
+      && (segment[0]['marketingAirline'] + segment[0]['flightNumber']) === lookup.flight) {
       // && segment[0].departureDateRaw.startsWith(LOOKUP.datetime)
       matchingFlight = segment[0];
     }
@@ -72,7 +72,7 @@ function encodeSeatsUrl(matchingFlight, bookId) {
 }
 
 void (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.emulate(device);
   await page.evaluate("delete window['webdriver']");
@@ -82,53 +82,26 @@ void (async () => {
     });
   });
 
-  let pageListeners = {};
-  page.on('response', async response => {
-    debug("got response " + response.url());
-    for (const url in pageListeners) {
-      if (response.url().indexOf(url) > 0) {
-        debug(response.status());
-        debug(response.url());
-        const json = await response.json();
-        pageListeners[url](json, response);
-        break;
-      }
-    }
-  });
-
-  function t(url) {
-    let resolveCallback, rejectCallback;
-    pageListeners[url] = function (json, response) {
-      resolveCallback([json, response]);
-    };
-    return new Promise((resolve, reject) => {
-      resolveCallback = resolve;
-      rejectCallback = reject;
-    });
-  }
-
-  const date = LOOKUP.datetime.split("T")[0];
-  let shop = `https://www.westjet.com/shop/?lang=en&type=search&origin=${LOOKUP.origin}&destination=${LOOKUP.dest}&adults=1&children=0&infants=0&outboundDate=${date}&returnDate=&companionvoucher=false&iswestjetdollars=false&promo=&currency=USD&caller=https%3A%2F%2Fwww.westjet.com%2Fen-us%2Findex`;
-  const searchPromise = t("/bookingservices/flightSearch");
+  const date = lookup.datetime.split("T")[0];
+  let shop = `https://www.westjet.com/shop/?lang=en&type=search&origin=${lookup.origin}&destination=${lookup.dest}&adults=1&children=0&infants=0&outboundDate=${date}&returnDate=&companionvoucher=false&iswestjetdollars=false&promo=&currency=USD&caller=https%3A%2F%2Fwww.westjet.com%2Fen-us%2Findex`;
   await page.goto(shop);
-  const searchResponse = await searchPromise;// page.waitForResponse(r => r.url().indexOf("/bookingservices/flightSearch") > 0, 3000);
-
-  const bookId = extractedBookId(searchResponse[1]);
-  debug(bookId);
-  const matchingFlight = await extractFlight(searchResponse[0]);
+  const searchResponse = await page.waitForResponse(r => r.url().indexOf("/bookingservices/flightSearch") > 0, 3000);
+  const matchingFlight = await extractFlight(searchResponse);
   if (matchingFlight == null) {
     console.log("Unable to find flight");
     return;
   }
   debug(matchingFlight);
+  const bookId = extractedBookId(searchResponse);
+  debug(bookId);
+
   const seatsPromise = page.goto(encodeSeatsUrl(matchingFlight, bookId));
   const resp = await page.waitForResponse(r => r.url().indexOf("/extendedSeatmap") > 0, 3000);
   const json = await resp.json();
+  const seatUnavailable = (s) => s.characteristics.NO_SEAT || s.characteristics.OFFERED_LAST || s.occuppied;
   json.aircraft.deck.cabin.rows.forEach(row => {
     let info = ("" + row.rowNumber).padStart(2, ' ') + "   "
-      + row.seats.map(s =>
-        (s.seatNumber + " " + ((s.characteristics.NO_SEAT || s.characteristics.OFFERED_LAST) ? "---" : s.prices[0]['price'])).padStart(15, ' ')
-      ).join(" ");
+      + row.seats.map(s => (s.seatNumber + " " + (seatUnavailable(s) ? "---" : s.prices[0]['price'])).padStart(15, ' ')).join(" ");
     console.log(info);
   });
   await seatsPromise;
